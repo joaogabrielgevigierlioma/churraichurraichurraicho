7c56a8bd2c9efc70069775c0d4fd158640c2ae5f
joaogabrielgevigierlioma <111295176+joaogabrielgevigierlioma@users.noreply.github.com>
2024-05-03 08:29:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,14 +11,18 @@ for(let i=0;i <botoes.length;i++){
 }
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-//Código omitido
+
 
 const contadores = document.querySelectorAll(".contador");
 contadores[0].textContent = "Contagem regressiva";
 
-//Código omitido
+
 const contadores = document.querySelectorAll(".contador"); 
-const tempoObjetivo1 = new Date("2024-10-05"); 
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+
 
 contadores[0].textContent = tempo0bjetivo1; 
 
@@ -34,4 +38,20 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+
+  }
+  
 
